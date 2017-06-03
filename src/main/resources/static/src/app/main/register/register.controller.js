2709b54bb5d6c89ee69fce361c7dd3cac6699d82
hyunbeen <bigpig93@naver.com>
2017-06-03 22:00:24 +0900

    Update register.controller.js

--- src/main/resources/static/src/app/main/register/register.controller.js
+++ src/main/resources/static/src/app/main/register/register.controller.js
@@ -15,37 +15,8 @@
         
         
         // Methods
-        //File upload
-        $scope.images = [];
-        //파일 처리
-        $scope.processFiles = function (uploadImages) {
-            angular.forEach(uploadImages, function (flowFile, i) {
-                var fileReader = new FileReader();
-                fileReader.onload = function (event) {
-                    var uri = event.target.result;
-                    $scope.images[i] = uri;
-                };
-                fileReader.readAsDataURL(flowFile.file);
-            });
-        };
-
-        //image upload
-        $scope.imageUpload = function (file) {
-
-            if (file != null) {
-                file.upload = Upload.upload({
-                    url: './api/member/imageUpload',
-                    method: 'POST',
-                    file: file
-                }).success(function (data) {
-                    console.log('Success');
-                });
-            }
-        };
         
-           
-        
-        
+     
 	vm.register_loginfun = function (username,email,password,location_share) {
         //user 회원정보 서버에 전송
 	    $http({
