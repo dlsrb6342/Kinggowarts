2711f3e147b08872a7c2f6e910cd01e345325192
dlsrb6342 <dlsrb6342@gmail.com>
2017-06-12 01:19:36 +0900

    공지사항 정렬 수정

--- src/main/java/com/kinggowarts/notice/NoticeSearchRepository.java
+++ src/main/java/com/kinggowarts/notice/NoticeSearchRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import java.util.List;
 
 public interface NoticeSearchRepository extends ElasticsearchRepository<Notice, Long>{
-    List<Notice> findAllByContentsContainsOrTitleContainsOrderByTimeDesc(String contents, String title);
+    List<Notice> findAllByContentsContainsOrTitleContainsOrderByTimeAsc(String contents, String title);
 }
